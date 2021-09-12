cd98d98a6982cde288f5287fa3916add81a5da04
BRB3D <samuraix221@hotmail.com>
2021-09-11 23:58:38 -0400

    Continuation of exercises with return + console

---
+++ even.js
@@ -0,0 +1,13 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+
+// In this case we used variables to store the returned value and then we printed it. but it is not necessary as the code below proves that we can just use the returned value;
+/* const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);  */
+
+console.log(isEven(10)); 
+console.log(isEven(11)); 
